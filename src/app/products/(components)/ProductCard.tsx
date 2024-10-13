d10e56980a86bae5a5f4274724e31965d17720b6
littshepherd <55956863+littshepherd@users.noreply.github.com>
2024-10-12 22:47:25 -0500

    Revert "Feature/list all by category (#2)" (#3)
    This reverts commit a2cba5feb393b352f6e7fe09dd0649363ef6face.

--- src/app/products/(components)/ProductCard.tsx
+++ src/app/products/(components)/ProductCard.tsx
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-=======
 import { Button } from "@mui/material";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Link from "next/link";
->>>>>>> dev
 import { usePathname } from "next/navigation";
 export default function ProductCard(
   { id, image, name, price } //: { image: any }
 ) {
-<<<<<<< HEAD
-  const pathname = usePathname();
-  const [imageData, setImageData] = useState("");
-
-=======
   const [imageData, setImageData] = useState("");
   const pathname = usePathname();
->>>>>>> dev
   const setImage = async (image) => {
     if (image) {
       const response = await fetch(`${image}`);
@@ -34,16 +22,6 @@ export default function ProductCard(
     setImage(image);
   }, [image]);
   return (
-<<<<<<< HEAD
-    <div className="w-80 bg-slate-700  rounded-md">
-      <div className="w-full h-80 p-2 ">
-        <Image
-          src={imageData}
-          alt="product"
-          width={200}
-          height={200}
-          className=" object-cover h-full w-full  rounded-t-md"
-=======
     <div className="w-80 bg-gray-600 rounded-md">
       <div className="w-full">
         <Image
@@ -52,21 +30,12 @@ export default function ProductCard(
           width={100}
           height={100}
           className="w-full"
->>>>>>> dev
         />
       </div>
       <div className="text-white p-4">
         <p>{name}</p>
         <p>Price: {price}</p>
       </div>
-<<<<<<< HEAD
-      <Link
-        href={`${pathname}/${id}`}
-        className="flex justify-center items-center h-12 w-full  bg-fuchsia-950 rounded-none rounded-b-md text-white"
-      >
-        Ver Producto
-      </Link>
-=======
       <Button
         sx={{
           textColor: "white",
@@ -84,7 +53,6 @@ export default function ProductCard(
           Ver Producto
         </Link>
       </Button>
->>>>>>> dev
     </div>
   );
 }
