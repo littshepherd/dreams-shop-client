a2cba5feb393b352f6e7fe09dd0649363ef6face
littshepherd <55956863+littshepherd@users.noreply.github.com>
2024-10-12 21:31:55 -0500

    Feature/list all by category (#2)
    * addign mui
    
    * products administration and product list
    
    * change card style and create the page of product by id without styles

--- src/app/products/(components)/ProductLIst.tsx
+++ src/app/products/(components)/ProductLIst.tsx
@@ -6,10 +6,10 @@ export default function ProductLIst(
 ) {
   return (
     <div className=" p-8 flex flex-row items-center flex-wrap gap-4">
-      {products.map((product) => (
+      {products.map((product, index) => (
         <ProductCard
           id={product.id}
-          key={product.id}
+          key={index}
           image={`${"https://pub-700600023f6f48fb88997df52195b1be.r2.dev/"}${
             product?.product_image[0]?.image_url ||
             "images/1727145686989images.jpeg"
